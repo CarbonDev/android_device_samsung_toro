fc3e895add69e78a355e694e6baaf7b938bb1fa0
Blueharford <jsseidel1@gmail.com>
2012-07-13 04:05:57 -0700

    build liquid for now
    Change-Id: Ib24a1416c0a6c9943c5fb3cbdb53bf4250301e73

---
+++ liquid.mk
@@ -0,0 +1,30 @@
+# name
+PRODUCT_RELEASE_NAME := GN-VZW
+
+# phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# device
+$(call inherit-product-if-exists, device/samsung/toro/full_toro.mk)
+
+# products
+PRODUCT_DEVICE := toro
+PRODUCT_BRAND := google
+PRODUCT_NAME := liquid_toro
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_PROPERTY_OVERRIDES += ro.modversion=liquid.toro.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+# overrides
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#    BUILD_ID=IMM76K \
+#    BUILD_NUMBER=336647 \
+#    PRODUCT_NAME=mysid \
+#    TARGET_BUILD_TYPE=user \
+#    BUILD_VERSION_TAGS=release-keys \
+#    PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys" \
+#    BUILD_FINGERPRINT="google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys"
+
+# media
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/media/xhdpi/bootanimation.zip:system/media/bootanimation.zip
