20ff29c322c1af097bf563caaad55e08d09501af
nocoast <voteforpedro8@gmail.com>
2012-09-25 17:39:15 -0500

    Updated build fingerprint.

--- liquid.mk
+++ liquid.mk
@@ -22,11 +22,5 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.toro.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # overrides
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_NUMBER=336647 \
-    PRODUCT_NAME=mysid \
-    TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="mysid-user 4.0.4 IMM76K 336647 release-keys" \
-    BUILD_FINGERPRINT="google/mysid/toro:4.0.4/IMM76K/336647:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys"
 
