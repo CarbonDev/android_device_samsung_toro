a08eb976df43045092f4baedd8e004b76f59fbd3
LiquidSmooth <liquid0624@gmail.com>
2012-09-27 23:16:46 -0400

    Fix Toro Build Prop Overrides

--- liquid.mk
+++ liquid.mk
@@ -22,5 +22,11 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.toro.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=424425 \
+    PRODUCT_NAME=mysid \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys" \
+    BUILD_FINGERPRINT="google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys"
 
